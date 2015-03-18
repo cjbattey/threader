92ac61a20a72958afe49acc20e361845ad9eea39
slager <dave.slager@gmail.com>
2015-03-18 00:25:04 -0700

    made threader.py more pythonic

--- threader.py
+++ threader.py
@@ -1,27 +1,39 @@
 #! /usr/bin/python
 
+##	This script spawns a user-defined number of threads to sequentially execute a list of shell calls.
+##	The shell calls are located in file threadme.txt and each shell call is on a separate line.
+##	It also tells you which thread completed which call and when each thread terminated.
+##	This is useful for things like multiple STRUCTURE runs.
+##	Don't forget to specify the number of threads on the next line!
+
+threads = 4		# User-defined number of threads
+
 import threading
 import os
 import subprocess
 
-## This script threads out the shell calls in file threadme.txt
-threads = 4    ## Specify total number of threads here
-
+# Only one thread at a time can open the lock
 lock = threading.Lock()
+
+# What to do within each spawned thread
 def threader():
+	# Start next call if threadme.txt still contains calls
 	while os.stat('threadme.txt').st_size >0:
-		## Run 1st call from threadme.txt, then delete that line
+		# Only one thread at a time may read/write threadme.txt
 		with lock:
-			f = open('threadme.txt','rb')
-			call = f.readline()
+			# Read the first call from threadme.txt
+			with open('threadme.txt','rb') as f:
+				call = f.readline()
+			# Delete the first call from threadme.txt
 			subprocess.call("tail -n +2 threadme.txt > smaller.tmp",shell=True)
 			os.rename("smaller.tmp","threadme.txt")
-			f.close()
+		# Send the call to the shell
 		subprocess.call(call,shell=True)
 		if call != "":
 			print(threading.current_thread().name + " did call: " + call)
 	print(threading.current_thread().name + " terminated")
-		
+
+# Spawn the threads		
 for t in range(threads):
 	threading.Thread(target = threader).start()
 	print("Thread-" + str(t+1) + " spawned")
